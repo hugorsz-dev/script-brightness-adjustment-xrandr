c7f103b876be7ae729ffd7303d429902536b3d28
hugorsz-dev <hugoruizschz@gmail.com>
2025-05-28 18:56:21 +0200

    Modified active_monitors variable, for a better compatibility with all display settings

--- brightness-adjustment-i3/decreasebrightness.sh
+++ brightness-adjustment-i3/decreasebrightness.sh
@@ -4,7 +4,7 @@ decrease=0.1
 current_brightness=`xrandr --verbose | grep Brightness | sed 's/[^0-9.]*//g' | head -n1`
 result_brightness=`bc -l <<< $current_brightness-$decrease`
 
-active_monitors=`xrandr --listactivemonitors | grep -o '[A-Z][A-Z]*-[0-9][0-9]*' | awk '!seen[$0]++'`
+active_monitors=`xrandr --listactivemonitors | grep ": +" |  awk '{print $NF}'`
 
 echo "$active_monitors" | tr ' ' '\n' | while read item; do
   xrandr --output $item --brightness $result_brightness
--- brightness-adjustment-i3/increasebrightness.sh
+++ brightness-adjustment-i3/increasebrightness.sh
@@ -4,7 +4,7 @@ increase=0.1
 current_brightness=`xrandr --verbose | grep Brightness | sed 's/[^0-9.]*//g' | head -n1`
 result_brightness=`bc -l <<< $current_brightness+$increase`
 
-active_monitors=`xrandr --listactivemonitors | grep -o '[A-Z][A-Z]*-[0-9][0-9]*' | awk '!seen[$0]++'`
+active_monitors=`xrandr --listactivemonitors | grep ": +" |  awk '{print $NF}'`
 
 echo "$active_monitors" | tr ' ' '\n' | while read item; do
   xrandr --output $item --brightness $result_brightness
